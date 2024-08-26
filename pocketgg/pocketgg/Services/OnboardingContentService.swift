1f1f17c3fa8bc93f6fde8fa094a2bd55b9f7f973
Gabriel Siu <gabrielsiu831@gmail.com>
2024-08-25 21:21:01 -0400

    Updated version 2.0 what's new text

--- pocketgg/pocketgg/Services/OnboardingContentService.swift
+++ pocketgg/pocketgg/Services/OnboardingContentService.swift
@@ -51,16 +51,54 @@ final class OnboardingContentService {
     [
       OnboardingContent(
         id: 0,
-        title: "APP UPDATED",
-        subtitle: "A video game tournament companion app, powered by start.gg",
+        title: "pocketgg updated to version 2.0",
+        subtitle: "Here's whats new in this update:",
         type: .image,
         imageName: "TODO",
         videoGames: nil
       ),
       OnboardingContent(
         id: 1,
-        title: "APP UPDATED 2",
-        subtitle: "A video game tournament companion app, powered by start.gg",
+        title: "OAuth Login Support",
+        subtitle: "You can now log in to pocketgg using your start.gg account, instead of having to manually create and copy an access token.",
+        type: .image,
+        imageName: "TODO",
+        videoGames: nil
+      ),
+      OnboardingContent(
+        id: 2,
+        title: "Improved Location Settings",
+        subtitle: """
+        You can now set a more precise location in pocketgg, to find tournaments close to your area. \
+        This feature requires Location Services to be enabled for pocketgg.
+        """,
+        type: .image,
+        imageName: "TODO",
+        videoGames: nil
+      ),
+      OnboardingContent(
+        id: 3,
+        title: "Improvements to Searching",
+        subtitle: """
+        Searching for tournaments now delivers much more accurate results. \
+        Also, you no longer have to send Video Game Update Requests; all video games on start.gg are now available on pocketgg.
+        """,
+        type: .image,
+        imageName: "TODO",
+        videoGames: nil
+      ),
+      OnboardingContent(
+        id: 4,
+        title: "Other Changes",
+        subtitle: """
+        • Tapping a set/match now shows the station and stream it's being played on (if available), as well as the seeds of both players
+        • Added color indicators to indicate which sets/matches are in progress
+        • Added support for Swiss brackets
+        • Added the ability to add a tournament directly to your calendar
+        • Added the ability to filter a TO's tournaments to those that they are directly organizing, are an admin of, or are competing in
+        • Added an 'Online' section to the main screen for all online tournaments
+        • Many other bug fixes and improvements
+        """,
         type: .image,
         imageName: "TODO",
         videoGames: nil
